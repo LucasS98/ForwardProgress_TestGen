e52b5d51f9b38eb8902d505b479b647eb56ba2a0
Chai <chai@princeton.edu>
2020-03-30 00:20:00 -0400

    Fixes

--- processing/RemoveENDSpace.sh
+++ processing/RemoveENDSpace.sh
@@ -8,11 +8,10 @@ fi
 shopt -s nullglob
 
 source="${BASH_SOURCE%/*}/"
-for f in $newname/*/
+for f in $1/*/
 do
 	for f2 in $f/*.txt
 	do
 		python $source/RemoveENDSpace.py $f2
-	fi
 	done 		
 done
