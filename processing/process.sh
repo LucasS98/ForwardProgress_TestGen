e0c050aa84c6d422aced526ca1135c0d6caf667f
LucasS98 <lucasfernansalvador@outlook.com>
2020-04-26 04:20:10 -0300

    Added CSV creation

--- processing/process.sh
+++ processing/process.sh
@@ -28,17 +28,21 @@ do
 	then
 		:
 	else
+		# Make Dot, PNG, PDF files
 		python $source/DotModify.py $f2
 		v2=${f2%.dot}
 		dot -Tpdf ${v2}_simple.dot -o $v2.pdf
 		dot -Tpng ${v2}_simple.dot -o $v2.png
 		cp $v2.xml ${v2}xml.txt
 
+		# Getting some useful variables for parameters
 		folderN=${f2%/*}
 		v3=${f2##*/}
 		index=${v3%.dot}
+		# Make files for CADP
 		python $source/CADPGen.py $folderN $index $source
 
+		# Process CADP files into labels
 		(cd $folderN/checker && lnt.open test_${index}_none.lnt generator test_${index}_none.bcg > /dev/null)
 		(cd $folderN/checker && lnt.open test_${index}_obe.lnt generator test_${index}_obe.bcg > /dev/null)
 		(cd $folderN/checker && lnt.open test_${index}_hsa.lnt generator test_${index}_hsa.bcg > /dev/null)
@@ -47,11 +51,14 @@ do
 		(cd $folderN/checker && svl test_${index}.svl > labels_${index}.txt)
 		python $source/ProcessLabels.py $folderN/checker/labels_${index}.txt
 
+		# Make dot files from CADP graphs (convert to PDF manually as needed)
 		(cd $folderN/checker && for i in *bcg ; do bcg_io $i -graphviz `basename $i .bcg`.dot ; done )
 
+		# Move files to a different folder to clean test folder somewhat
 		(cd $folderN/checker && cp labels_${index}.txt ../label_${index}.txt)
 		(cd $folderN && mv checker ../../checker_files/$index)
 
+		# Make Amber file (no saturation) for test (thanks Hari!)
 		python $source/amber_test_generation.py $v2.txt $v2
 
 		count=`expr $count + 1`
@@ -59,4 +66,12 @@ do
 	done 		
 done
 
+# Convert scheduler results to CSV for comparison
+python $source/CreateCSV.py $newname OBE
+python $source/CreateCSV.py $newname HSA
+python $source/CreateCSV.py $newname HSA_OBE
+python $source/CreateCSV.py $newname LOBE
+python $source/CreateCSV.py $newname WEAK_FAIR
+
+# Copy HTML (with appropriate changes) to test folder
 python $source/HTMLGen.py $source/testExplorer.html $newname/testExplorer.html $count
